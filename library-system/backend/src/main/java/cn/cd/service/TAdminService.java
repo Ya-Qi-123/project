8dca68103a8e72f8105fd24488528d6c58521e57
TianQize <3063400699@qq.com>
2025-06-21 11:32:53 +0800

    更新admin的用户名密码登录功能

--- library-system/backend/src/main/java/cn/cd/service/TAdminService.java
+++ library-system/backend/src/main/java/cn/cd/service/TAdminService.java
@@ -1,13 +1,7 @@
 package cn.cd.service;
 
 import cn.cd.domain.TAdmin;
-import com.baomidou.mybatisplus.extension.service.IService;
 
-/**
-* @author Lenovo
-* @description 针对表【t_admin】的数据库操作Service
-* @createDate 2025-06-21 09:22:10
-*/
-public interface TAdminService extends IService<TAdmin> {
-
+public interface TAdminService {
+    TAdmin getByAdminnameAndPassword(String adminname, String password);
 }
