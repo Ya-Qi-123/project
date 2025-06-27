a8c0abd2bce064dab19e149a252cddabaa583a85
TianQize <3063400699@qq.com>
2025-06-27 09:33:19 +0800

    Delete LendService.java

--- library-system/src/main/java/cn/cd/service/LendService.java
+++
@@ -1,6 +0,0 @@
-import com.baomidou.mybatisplus.extension.service.IService;
-import java.util.List;
-
-public interface LendService extends IService<TLendrecord> {
-    Integer getStatus(Long user_id);
-}
