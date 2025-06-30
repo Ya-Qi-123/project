9e28d90958c19424c1ad654a7feb416942154cab
jerryDeng7 <139206037+jerryDeng7@users.noreply.github.com>
2025-06-30 10:34:11 +0800

    修改favor自增长

--- library-system/backend/src/main/java/cn/cd/domain/TFavor.java
+++ library-system/backend/src/main/java/cn/cd/domain/TFavor.java
@@ -1,12 +1,15 @@
 package cn.cd.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
 @TableName("t_favorite")
 public class TFavor {
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     @TableField("isbn")
