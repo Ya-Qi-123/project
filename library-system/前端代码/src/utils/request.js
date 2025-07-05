389b193d7457292407f2b49dc9c90d1321fd8ba6
xia-yi-xin <2227980358@qq.com>
2025-07-05 10:16:43 +0800

    Delete library-system/前端代码 directory

--- library-system/前端代码/src/utils/request.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const service = axios.create({
-    //baseURL: 'http://192.168.10.37:8080',
-    //baseURL:'http://localhost:8080',
-    timeout: 10000
-})
-export default service
--- library-system/前端代码/src/views/dynatest/component1.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>页面1</h1>
-    <p>这是页面1的内容</p>
-  </div>
-</template>
